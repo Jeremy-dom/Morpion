eb4bba4821b72b5e2ffa86f24528338031ff8ae0
djeremy33 <78786641+djeremy33@users.noreply.github.com>
2024-01-01 21:18:36 +0100

    Addition of the graphic library
    Add all the components to use the SFML library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+// #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 #include "main.h"
 
 int main(int, char**){
-    std::cout << "Hello, from Morpion!\n";
+    auto window = sf::RenderWindow{ { WINDOW_L, WINDOW_H }, "Morpion" };
+    window.setFramerateLimit(FPS_LIMIT);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+    return 0;
 }
